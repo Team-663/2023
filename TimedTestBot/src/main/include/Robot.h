6e480f0737df8dbf102757f1e540d0d18dc8ab4c
dravis123 <randolph.ben@gmail.com>
2023-03-11 14:33:32 -0500

    added a test bot for worcester academy

---
+++ TimedTestBot/src/main/include/Robot.h
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/XboxController.h>
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  frc::PWMSparkMax m_leftMotor{1};
+  frc::PWMSparkMax m_rightMotor{2};
+  frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
+  frc::XboxController m_xbox{2};
+};
